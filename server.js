fa4b4e21090ee0b23f65bc2ae4c506ade3e6edbd
SemosKg <Samuelkumelag@gmail.com>
2025-08-11 12:48:26 +0300

    e

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const notesRoutes = require('./routes/notes');
 const app = express();
 
-app.use(express.json()); 
+app.use(express.json());
 
 app.use('/api/notes', notesRoutes);
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
-//u are semos
+// ato aman
