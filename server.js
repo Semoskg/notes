f937e0555919dc9536461474316a041f412fe5a3
SemosKg <Samuelkumelag@gmail.com>
2025-08-12 10:16:37 +0300

    added the .env

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const notesRoutes = require('./routes/notes');
 const app = express();
@@ -6,7 +7,7 @@ app.use(express.json());
 
 app.use('/api/notes', notesRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server started on port 3000');
 });
 // ato aman
